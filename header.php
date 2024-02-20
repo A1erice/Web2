cf283e6b19bf6bc1ae5b93b3bb03cf2cf02b1037
A1erice <128226905+A1erice@users.noreply.github.com>
2024-02-20 14:29:05 +0700

    đổi sang php

--- header.php
+++ header.php
@@ -1,23 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>SickShoeShop</title>
-  <!-- style css -->
-  <link rel="stylesheet" href="../assets/css/bootstrap_css/bootstrap.min.css" />
-  <link rel="stylesheet" href="../assets/css/main_css/style.css" />
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
-    rel="stylesheet" />
-
-  <!-- BOOTSTRAP ICON CDN -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-</head>
-
-<body>
   <!-- HEADER -->
   <header>
     <!-- NAVBAR -->
@@ -80,7 +61,7 @@
       </div>
       <div class="carousel-inner">
         <div class="carousel-item active" data-interval="1000">
-          <div class="overlay-image" style="background-image: url('../assets/img/banner1.avif')">
+          <div class="overlay-image" style="background-image: url('assets/img/banner1.avif')">
           </div>
           <div class="container">
             <h2>It's Never Too Late</h2>
@@ -91,7 +72,7 @@
         </div>
 
         <div class="carousel-item" data-interval="500">
-          <div class="overlay-image" style="background-image: url('../assets/img/banner2.avif')">
+          <div class="overlay-image" style="background-image: url('assets/img/banner2.avif')">
           </div>
           <div class="container">
             <h2>New Collection Sneakers</h2>
@@ -102,7 +83,7 @@
         </div>
 
         <div class="carousel-item" data-interval="500">
-          <div class="overlay-image" style="background-image: url('../assets/img/banner3.avif')">
+          <div class="overlay-image" style="background-image: url('assets/img/banner3.avif')">
           </div>
           <div class="container">
             <h2>Find Out Now</h2>
@@ -125,90 +106,3 @@
     </div>
     <!-- end slide carousel -->
   </header>
-
-
-
-
-  <!-- FOOTER -->
-
-  <footer class="footer bg-primary text-white pt-5 pb-4">
-    <div class="container-fluid text-center text-md-left">
-      <div class="row text-center text-md-left">
-
-        <div class="col-md-3 col-lg-3 mx-auto mt-3">
-          <h5 class="text-uppercase mb-4 ">SHOP NAME</h5>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quidem hic quam magnam ratione, quibusdam
-            tempora itaque repellendus unde dolore quae consectetur aperiam nostrum velit tempore? Minus quas
-            accusantium iusto!</p>
-        </div>
-
-        <div class="col-md-2 col-lg-2 mx-auto mt-3">
-          <h5 class="text-uppercase mb-4 ">My Account</h5>
-          <p>
-            <a href="" class="text-white">Sign In</a>
-          </p>
-          <p>
-            <a href="" class="text-white">My Wishlist</a>
-          </p>
-          <p>
-            <a href="" class="text-white">View Cart</a>
-          </p>
-          <p>
-            <a href="" class="text-white">Help</a>
-          </p>
-        </div>
-
-        <div class="col-md-3 col-lg-2 mx-auto mt-3">
-          <h5 class="text-uppercase mb-4">About Us</h5>
-          <p>
-            <a href="" class="text-white">Privacy Policy</a>
-          </p>
-          <p>
-            <a href="" class="text-white">Terms and Conditions</a>
-          </p>
-          <p>
-            <a href="" class="text-white">Support</a>
-          </p>
-          <p>
-            <a href="" class="text-white">Delivery Infomation</a>
-          </p>
-        </div>
-
-        <div class=" col-md-4 col-lg-3 mx-auto mt-3">
-          <h5 class="text-uppercase mb-4">Contact US</h5>
-          <p>
-            <a href="" class="text-white bi-envelope" style="text-decoration: none;"> nhomweb@gmail.com</a>
-          </p>
-          <p>
-            <a href="" class="text-white bi-phone">+123456789</a>
-          </p>
-          <p>
-            <a href="" class="text-white bi-hourglass">9:00 - 21:00,Mon - Sat</a>
-          </p>
-          <p>
-            <a href="" class="text-white bi-facebook"></a>
-            <a href="" class="text-white bi-instagram"></a>
-            <a href="" class="text-white bi-twitter-x"></a>
-            <a href="" class="text-white bi-youtube"></a>
-          </p>
-        </div>
-      </div>
-
-      <hr>
-      <div class="row text-center text-md-left">
-        <h5>Create with <i class="bi-heart-fill"></i> by <i class="bi bi-c-circle"></i>SickShoesShop</h5>
-      </div>
-    </div>
-  </footer>
-
-  <!-- END FOOTER -->
-
-
-</body>
-
-</html>
-
-<script src="../assets/js/bootstrap_js/bootstrap.min.js"></script>
-</body>
-
-</html>
