49fe1cd65c3a13de434864a1bcbeb4e79bf009bc
lethienphuc1 <157466550+lethienphuc1@users.noreply.github.com>
2024-05-07 16:33:58 +0700

    xem hóa đơn người dùng

--- app/controllers/frontend/order.php
+++ app/controllers/frontend/order.php
@@ -8,10 +8,18 @@ class Order extends Controller
     $user_data = $user->check_login();
     if (!is_null($user_data)) {
       $data['user_data'] = $user_data;
-    }
-    $data['page_title'] = "Order";
+      $data['page_title'] = "Order";
     $this->view("frontend/order", $data);
+    }
+    
   }
 
-
+function getAll()
+{  if($_SERVER['REQUEST_METHOD'] =='POST')
+  {
+    $orders=$this->model('frontend/OrderModel');
+    $orders->GetAll($_POST);  
+  
+   }
+ }
 }
--- app/views/frontend/order.php
+++ app/views/frontend/order.php
@@ -4,91 +4,35 @@
     <div class="col-12 d-flex align-items-center justify-content-between mb-3">
       <h6 class="mb-3">Đơn hàng của bạn</h6>
     </div>
-    <div class="col-12 table-responsive mb-3">
-      <table class="table text-start align-middle table-bordered table-hover mb-0">
-        <thead>
-          <tr class="text-dark">
-            <th scope="col"><input class="form-check-input" type="checkbox"></th>
-            <th scope="col">Date</th>
-            <th scope="col">Invoice</th>
-            <th scope="col">Customer</th>
-            <th scope="col">Amount</th>
-            <th scope="col">Status</th>
-            <th scope="col">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td><input class="form-check-input" type="checkbox"></td>
-            <td>01 Jan 2045</td>
-            <td>INV-0123</td>
-            <td>Jhon Doe</td>
-            <td>$123</td>
-            <td>Paid</td>
-            <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
-          </tr>
-          <tr>
-            <td><input class="form-check-input" type="checkbox"></td>
-            <td>01 Jan 2045</td>
-            <td>INV-0123</td>
-            <td>Jhon Doe</td>
-            <td>$123</td>
-            <td>Paid</td>
-            <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
-          </tr>
-          <tr>
-            <td><input class="form-check-input" type="checkbox"></td>
-            <td>01 Jan 2045</td>
-            <td>INV-0123</td>
-            <td>Jhon Doe</td>
-            <td>$123</td>
-            <td>Paid</td>
-            <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
-          </tr>
-          <tr>
-            <td><input class="form-check-input" type="checkbox"></td>
-            <td>01 Jan 2045</td>
-            <td>INV-0123</td>
-            <td>Jhon Doe</td>
-            <td>$123</td>
-            <td>Paid</td>
-            <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
-          </tr>
-          <tr>
-            <td><input class="form-check-input" type="checkbox"></td>
-            <td>01 Jan 2045</td>
-            <td>INV-0123</td>
-            <td>Jhon Doe</td>
-            <td>$123</td>
-            <td>Paid</td>
-            <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    <div class="col-12 pb-1">
-      <nav aria-label="Page navigation">
-        <ul class="pagination justify-content-center mb-3">
-          <li class="page-item disabled">
-            <a class="page-link" href="#" aria-label="Previous">
-              <span aria-hidden="true">&laquo;</span>
-              <span class="sr-only">Previous</span>
-            </a>
-          </li>
-          <li class="page-item active"><a class="page-link" href="#">1</a></li>
-          <li class="page-item"><a class="page-link" href="#">2</a></li>
-          <li class="page-item"><a class="page-link" href="#">3</a></li>
-          <li class="page-item">
-            <a class="page-link" href="#" aria-label="Next">
-              <span aria-hidden="true">&raquo;</span>
-              <span class="sr-only">Next</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
+    <div id="displayOrderData">
+
+
+
     </div>
   </div>
 
 
 </div>
 <?php $this->view("include/footer", $data) ?>
+<script>
+  function fetch_data(page) 
+  {  var user_id = <?= $data['user_data']->id ?>;
+      $.ajax({
+             url:'<?= ROOT ?>order/getAll',
+             method: "POST",
+             data :{
+              page:page,
+              user_id: user_id
+             },
+             success: function(data,status){
+              console.log(data);
+              $('#displayOrderData').html(data);
+             } 
+            });    
+  }
+  fetch_data();
+  function changePageFetch(page) {
+    fetch_data(page);
+  }
+  
+</script>
